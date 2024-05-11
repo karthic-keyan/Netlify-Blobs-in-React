35ab4e399fed6ce53e6960f9bce67447fefad9e2
karthikeyan <karthikn2895@gmail.com>
2024-05-11 19:42:19 +0530

    update

--- src/indexDB.js
+++ src/indexDB.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { setEnvironmentContext, getStore, listStores } from "@netlify/blobs";
+import { getDeployStore } from "@netlify/blobs";
 // Constants for IndexedDB configuration
 const DB_NAME = "BlobDatabase";
 const STORE_NAME = "blobStore";
@@ -10,31 +11,46 @@ const IndexedDBBlob = () => {
     const [selectedFile, setSelectedFile] = useState(null);
     const [downloadLink, setDownloadLink] = useState(null);
 
+    const store = getDeployStore({
+        siteID: "de897f5a-f03e-44e3-9d71-2f6c2c55a40c",
+        token: "nfp_DxkBw4dXtypyurZtwcTUt1oCH8UrK5SU76ad",
+        deployID : "6637b03d4e766aede338b245"
+      });
+      const { blobs } = store.list();
+      console.log('blobs', blobs)
+    // const store = getStore({ siteID: 'de897f5a-f03e-44e3-9d71-2f6c2c55a40c', token: 'nfp_DxkBw4dXtypyurZtwcTUt1oCH8UrK5SU76ad', name: 'foo' });
+    console.log('store', store)
+      const getList = async () => {
+        const { blobs } = await store.list();  
+        console.log('blobs', blobs)
+      }
+      getList()
     // Initialize IndexedDB
   
-    const sampleData = getStore("demo")
+    // const sampleData = getStore("demo")
     // Handle file selection
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     };
 
-    // Store the selected file in IndexedDB
+     
+    // // Store the selected file in IndexedDB
     const storeBlob = async () => {
-        await sampleData.set("demo",selectedFile );
+        await store.setJSON("nails", { type: "common", finish: "bright" });
     };
 
-    // Retrieve the stored blob from IndexedDB and create a download link
-    const retrieveBlob = async () => {
-        const data = await sampleData.set("demo");
-        console.log('data', data)
-    };
+    // // Retrieve the stored blob from IndexedDB and create a download link
+    // const retrieveBlob = async () => {
+    //     const data = await sampleData.set("demo");
+    //     console.log('data', data)
+    // };
 
     return (
         <div>
             <h2>IndexedDB Blob Storage Example</h2>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={storeBlob}>upload File</button>
-            <button onClick={retrieveBlob}>Retrieve File</button>
+             <input type="file" onChange={handleFileChange} />
+             <button onClick={storeBlob}>upload File</button>
+            {/* <button onClick={retrieveBlob}>Retrieve File</button>   */}
 
             {downloadLink && (
                 <div>
